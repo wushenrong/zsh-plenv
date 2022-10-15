2482bbd5bd71c4af27dfc7a312cde94862e61841
Samuel Wu 吴申榕 <twopizza9621536@gmail.com>
2022-10-14 22:49:09 -0400

    Inital Commit
    Signed-off-by: Samuel Wu 吴申榕 <twopizza9621536@gmail.com>

---
+++ README.md
@@ -0,0 +1,29 @@
+# zsh-plenv
+
+A zsh plugin for plenv based on the jenv plugin.
+
+## Usage
+
+To use it, install [`plenv`](https://github.com/pyenv/pyenv) first.
+
+Then clone this repository into the custom plugins directory as `plenv` of your
+zsh plugin manager which is `$ZSH_CUSTOM`, the following is for
+[Oh My Zsh](https://ohmyz.sh/) which is `~/.oh-my-zsh/custom/plugins`.
+
+```bash
+git clone https://github.com/TwoPizza9621536/zsh-plenv.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/plenv
+```
+
+Finally, add pyenv to the plugins array of your zshrc file:
+
+```bash
+plugins=(... plenv)
+```
+
+## License
+
+Under the MIT License with the following SPDX expression:
+
+```text
+SPDX-License-Identifier: MIT
+```
---
+++ plenv.plugin.zsh
@@ -0,0 +1,36 @@
+# SPDX-License-Identifier: MIT
+plenvdirs=("$HOME/.plenv" "/usr/local/bin/plenv" "/usr/local/plenv" "/opt/plenv")
+
+FOUND_PLENV=0
+for plenvdir in $plenvdirs; do
+    if [[ -d "${plenvdir}/bin" ]]; then
+        FOUND_PLENV=1
+        break
+    fi
+done
+
+if [[ $FOUND_PLENV -eq 0 ]]; then
+    if (( $+commands[brew] )) && plenvdir="$(brew --prefix plenv)"; then
+        [[ -d "${plenvdir}/bin" ]] && FOUND_PLENV=1
+    fi
+fi
+
+if [[ $FOUND_PLENV -eq 1 ]]; then
+    (( $+commands[plenv] )) || export PATH="${plenvdir}/bin:$PATH"
+    eval "$(plenv init - zsh)"
+
+    function plenv_prompt_info() {
+      local version="$(plenv version-name 2>/dev/null)"
+      echo "${version:gs/%/%%}"
+    }
+
+    if [[ -d "${plenvdir}/versions" ]]; then
+        export PLENV_ROOT=$plenvdir
+    fi
+else
+    function plenv_prompt_info() {
+      echo "Not Implemented"
+    }
+fi
+
+unset plenvdir plenvdirs FOUND_PLENV
